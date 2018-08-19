661e3edd5f51f87802e131e7e5552b69a91874f0
github username <kenny.hom27@gmail.com>
2018-08-18 22:59:44 -0400

    added welcome and header

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <h1>
+      Welcome, Sign in or Sign up!
+    </h1>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Welcome from './components/Welcome';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <App/>, document.querySelector('#root')
-)
+  <BrowserRouter>
+    <App>
+      <Route exact path="/" component={Welcome} />
+    </App>
+  </BrowserRouter>,
+  document.querySelector('#root')
+);
