ff61678c116a6f3cccaf8f1c94efb3be92a2c85f
Kenny <kenny.hom@bchainco.com>
2018-08-23 17:00:56 -0400

    added auth / redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import Welcome from './components/Welcome';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+
+import reducers from "./reducers";
+import App from "./components/App";
+import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route exact path="/" component={Welcome} />
-    </App>
-  </BrowserRouter>,
-  document.querySelector('#root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/signup" component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector("#root")
 );
