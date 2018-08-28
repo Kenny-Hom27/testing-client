57b534818ed633afc3b307d6b75f66bf27dc1df5
github username <kenny.hom27@gmail.com>
2018-08-27 22:19:04 -0400

    added css and auth features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-import reducers from "./reducers";
-import App from "./components/App";
-import Welcome from "./components/Welcome";
-import Feature from "./components/Feature";
-import Signup from "./components/auth/Signup";
+import reducers from './reducers';
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Feature from './components/Feature';
+import Signup from './components/auth/Signup';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(
+  reducers,
+  {
+    auth: { authenticated: localStorage.getItem('token') }
+  },
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,8 +28,10 @@ ReactDOM.render(
         <Route exact path="/" component={Welcome} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/feature" component={Feature} />
+        <Route exact path="/signout" component={Signout} />
+        <Route exact path="/signin" component={Signin} />
       </App>
     </BrowserRouter>
   </Provider>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
